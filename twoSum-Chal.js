50dd64d9dfebb03f3d75a3fd6040c4e3732e7d7c
Code4Blessings <robinwarden@code4blessings.com>
2020-06-29 11:35:56 -0400

    Refactored code to optimize time complexity, but failed test

--- twoSum-Chal.js
+++ twoSum-Chal.js
@@ -10,15 +10,39 @@
 // return [0, 1].
 
 function twoSum(nums, target) {
+    //Questions:  Is the array sorted? Will an unsorted array array work well with this algorithm? => Unsorted arrays will return undefined.  Need to create an isSorted function.
+
     //1. Loop through the nums array and if 2 values equal the target, return the indices of their values.
-    for (let i=0; i < nums.length; i++) {
-        for(let j=1; j < nums.length; j++){
-            if (nums[i] + nums[j] === target) {
-                return [i, j]
-            }
+    //2. Make one variable,  starting at the left right at the first index(0)
+    let left = 0;
+    //3. make another variable starting at the right, which will be at the other end of the array.
+    let right = nums.length - 1;
+    //4. Now loop through the array.  While left is less than right, if the sum of the left and the right equals the target, then return the indices of their values.
+    for (i = 0; i < nums.length; i++) {
+        nums.sort();
+    }
+    while(left < right) {
+        if (nums[left] + nums[right] === target) {
+            return [left, right]
+        }else if(nums[left] + nums[right] !== target) {
+            right --
+        }else {
+            left ++
         }
     }
+    //5. If not decrement the index of the value on the right
+    //6. And increment the value of the index on the left.
+
+
+
+    // for (let i=0; i < nums.length; i++) {
+    //     for(let j=1; j < nums.length; j++){
+    //         if (nums[i] + nums[j] === target) {
+    //             return [i, j]
+    //         }
+    //     }
+    // }
 
 }
 
-console.log(twoSum([2, 7, 11, 15], 9))
+console.log(twoSum([3,2,4], 6))
